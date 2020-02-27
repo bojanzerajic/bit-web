1f12314efa758d5567aa79db177dabbf9f12b66c
BIT Student <classroom@bgit.rs>
2020-02-27 17:57:12 +0100

    BOM

---
+++ JS_in_browser/jsfile.js
@@ -0,0 +1,88 @@
+// var welcome = "Hello World";
+// console.log(welcome);
+
+
+////////////////////// NAVIGATION ///////////////////////
+// function navigation() {
+//     console.log(navigator.platform);
+//     console.log(navigator.appVersion);
+//     console.log(navigator.vendor);
+
+// }
+// navigation();
+
+// function isOnline() {
+//     console.log(navigator.onLine);
+// }
+
+// isOnline();
+
+
+////////////////////// SCREEN ///////////////////////
+// function screenInfo() {
+//     console.log(screen.width, screen.height, screen.availHeight);
+// }
+// screenInfo();
+
+
+////////////////////// LOCATION ///////////////////////
+// function urlInfo() {
+//     console.log(location.href);
+//     console.log(location.protocol);
+//     console.log(location.hostname);
+//     console.log(location.search);
+// }
+// urlInfo();
+
+// function newPage() {
+//     console.log(location.href = "https://www.bgit.rs/");
+
+// }
+// newPage();
+
+// function reloadPage() {
+//     console.log(location.reload());
+
+// }
+// reloadPage();
+
+
+////////////////////// STORAGE ///////////////////////
+
+// function storePassedData() {
+//     localStorage.setItem("name", "bit");
+// }
+// storePassedData();
+
+// //
+// function readStoredData() {
+//     var name = localStorage.getItem('name');
+
+//     if (name) {
+//         console.log(name)
+//     } else {
+//         console.log("There is no name stored")
+//     }
+// }
+// readStoredData();
+
+// //
+// function removeData() {
+//     localStorage.removeItem("name");
+
+
+// }
+// removeData();
+// readStoredData();
+
+//////////////////////////////
+
+// function greetingMessage() {
+//     alert('Ciao raggazzi');
+//     var answer = prompt('Stefane, koliko si umoran na skali od 1 do 10?');
+//     if (answer) {
+//         confirm("Your answer is " + answer);
+//     }
+// }
+
+// greetingMessage();
