45468a75f655a5a7db710d73b31c3178b814a87e
BIT Student <classroom@bgit.rs>
2020-03-02 12:42:51 +0100

    DOM exercise

---
+++ DOM_exercise/dom.js
@@ -0,0 +1,28 @@
+// var form = document.querySelector('form');
+
+// function addElement(type, label, required) {
+//     var input = document.createElement("input");
+//     var labelEl = document.createElement("label");
+//     var text = document.createTextNode(label);
+
+//     input.setAttribute("type", type);
+//     labelEl.appendChild(text);
+
+//     if (required) {
+//         input.setAttribute("required", "required");
+//     }
+
+//     form.appendChild(labelEl);
+//     form.appendChild(input);
+
+// }
+
+// addElement("password", "Password");
+// addElement("email", "Email", true);
+// addElement("text", "Name");
+
+
+
+
+
+
