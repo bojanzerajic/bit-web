83ef1a36fbc35291244fb5916aa5fe9716abece4
unknown <Bojan Zerajic>
2020-03-21 11:37:37 +0100

    Rick and Morty done

--- Rick_and_Morty/JS/characterInfo.js
+++ Rick_and_Morty/JS/characterInfo.js
@@ -1,1 +1,36 @@
 
+let a = window.location.search;
+
+let urlId = (function index() {
+    for (let i = 0; i < a.length; i++) {
+        return a.slice(4);
+
+    }
+})();
+console.log(urlId);
+
+let request = new XMLHttpRequest();
+request.open("GET", "https://rickandmortyapi.com/api/character/"+ urlId);
+request.send();
+
+request.onload = function() {
+    let response = JSON.parse(request.responseText);
+    console.log(response);
+    let image = document.querySelector('#image');
+    image.setAttribute('src', response.image);
+    let name = document.querySelector('#name');
+    name.textContent = response.name;
+    let status = document.querySelector('#status');
+    status.textContent = response.status;
+    let species = document.querySelector('#species');
+    species.textContent = response.species;
+    let type = document.querySelector('#type');
+    type.textContent = response.type;
+    let gender = document.querySelector('#gender');
+    gender.textContent = response.gender;
+    let location = document.querySelector('#location');
+    location.textContent = response.location.name;
+
+    
+    
+}
--- Rick_and_Morty/JS/main.js
+++ Rick_and_Morty/JS/main.js
@@ -4,26 +4,10 @@ request.send();
 request.onload = function () {
     let response = JSON.parse(request.responseText);
     console.log(response);
-    for (let i = 0; i < 20; i++) {
-        let list = document.createElement('ul');
-        list.className = 'hidden';
-        let status = document.createElement('li');
-        status.textContent = 'status  ' + response.results[i].status;
-        let species = document.createElement('li');
-        species.textContent = 'species    ' + response.results[i].species;
-        let type = document.createElement('li');
-        type.textContent = 'type    ' + response.results[i].type;
-        let gender = document.createElement('li');
-        gender.textContent = 'gender    ' + response.results[i].gender;
-        let location = document.createElement('li');
-        location.textContent = 'location    ' + response.results[i].location.name;
-        list.appendChild(status);
-        list.appendChild(species);
-        list.appendChild(type);
-        list.appendChild(gender);
-        list.appendChild(location);
-        let title = document.createElement('h6');
+    for (let i = 0; i < 20; i++) { 
+        let title = document.createElement('a');
         title.textContent = response.results[i].name;
+        title.setAttribute('href', "characterInfo.html?id=" + response.results[i].id);
         let image = document.createElement('img');
         image.setAttribute('src', response.results[i].image);
         let likeButton = document.createElement('div');
@@ -34,21 +18,34 @@ request.onload = function () {
         titleDiv.appendChild(likeButton);
         let singleCharacterDiv = document.createElement('div');
         singleCharacterDiv.appendChild(image);
-        singleCharacterDiv.appendChild(list);
         singleCharacterDiv.appendChild(titleDiv);
         singleCharacterDiv.className = 'singleCharacterDiv';
         let mainDiv = document.querySelector('.container');
         mainDiv.appendChild(singleCharacterDiv);
+        
     }
-    
+   
 }
-
-let getFullData = function () { 
-    let fullInfo = document.querySelector("ul");
-    fullInfo.removeClass = 'hidden';
-    fullInfo.className = 'active';
-}
-
-
-let likeButton = document.querySelector('.likeButton');
-likeButton.addEventListener('click', getFullData);
+// let button = document.querySelector('button');
+//         button.addEventListener('click', function () {
+//             let list = document.createElement('ul');
+//             let status = document.createElement('li');
+//             status.textContent = 'status  ' + response.results[i].status;
+//             let species = document.createElement('li');
+//             species.textContent = 'species    ' + response.results[i].species;
+//             let type = document.createElement('li');
+//             type.textContent = 'type    ' + response.results[i].type;
+//             let gender = document.createElement('li');
+//             gender.textContent = 'gender    ' + response.results[i].gender;
+//             let location = document.createElement('li');
+//             location.textContent = 'location    ' + response.results[i].location.name;
+//             list.appendChild(status);
+//             list.appendChild(species);
+//             list.appendChild(type);
+//             list.appendChild(gender);
+//             list.appendChild(location);
+//             let listDiv = document.createElement('div');
+//             listDiv.className = 'listDiv';
+//             listDiv.appendChild(list);
+//             singleCharacterDiv.appendChild(listDiv);
+//         });
